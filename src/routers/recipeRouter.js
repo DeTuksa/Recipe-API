b9c1149c4e921e402dbc40a7b1fed86f49b46962
DeTuksa <emmydave414@gmail.com>
2022-06-24 18:48:55 +0100

    api to fetch 10 random recipes

--- src/routers/recipeRouter.js
+++ src/routers/recipeRouter.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = new express.Router();
 const Recipe = require('../models/recipe');
+const axios = require('axios');
 
 const auth = require('../middleware/auth');
+
+const options = {
+    method: 'GET',
+    url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random',
+    params: {number: '10'},
+    headers: {
+      'X-RapidAPI-Key': '7c4d397837msha39637ef1f5e2ffp1f57eajsnb7c05a257e80',
+      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    }
+  };
+
+  //Route to get 10 random recipes
+router.get('/recipes/get-recipe', async (req, res) => {
+
+    try {
+        axios.request(options).then(function (response) {
+            res.status(200).send({
+                message: 'Recipes fetched successfully',
+                recipes: response.data.recipes
+                })
+        }).catch(function (error) {
+            return res.status(400).send(error)
+        });
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
 
 //Route to create a new recipe
 router.post('/create-recipe', auth, async (req, res) => {
