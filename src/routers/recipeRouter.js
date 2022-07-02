68fae04e49eef57cb75636f2969228b1d2f3699c
DeTuksa <emmydave414@gmail.com>
2022-07-02 14:43:54 +0100

    implemented searching recipe by text

--- src/routers/recipeRouter.js
+++ src/routers/recipeRouter.js
@@ -31,6 +31,35 @@ router.get('/recipes/get-recipe', async (req, res) => {
     }
 })
 
+  //Route to search recipe based on query
+  router.get('/recipes/search-recipe', async (req, res) => {
+    console.log(req.query)
+    const options = {
+        method: 'GET',
+        url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch',
+        params: {
+            query: req.query.query,
+          },
+        headers: {
+          'X-RapidAPI-Key': process.env.RECIPE_API_KEY,
+          'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+        }
+      };
+
+    try {
+        axios.request(options).then(function (response) {
+            res.status(200).send({
+                message: 'Search completed successfully',
+                recipes: response.data.results
+                })
+        }).catch(function (error) {
+            return res.status(400).send(error)
+        });
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 //Route to create a new recipe
 router.post('/create-recipe', auth, async (req, res) => {
 
